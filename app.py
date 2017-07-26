4e97990abc8468b9329059003a8fb8f324dc0c50
jhofeloto <jhofeloto@41gu5.com>
2017-07-25 20:51:44 -0500

    Dos tarjetas

--- app.py
+++ app.py
@@ -96,6 +96,24 @@ def makeWebhookResult(req):
                                             "title": "Ver3"
                                         }
                                     ]
+                                },
+                                {
+                                    "title": "Resident Evil: The Final Chapter (2017)",
+                                    "image_url": "https://www.moovrika.com/ext/makeimg.php?tbl=movies&id=4167&img=1&type=image&movie=Resident+Evil+The+Final+Chapter&fs=400",
+                                    "subtitle": "Resident Evil: The Final Chapter is an upcoming science fiction action horror film written and directed by Paul W. S. Anderson. It is the sequel to Resident Evil: Retribution (2012), and will be the sixth and final installment in the Resident Evil film series, which is very loosely based on the Capcom survival horror video game series Resident Evil.",
+                                    "default_action": {
+                                        "type": "web_url",
+                                        "url": "https://www.moovrika.com/m/4167",
+                                        "webview_height_ratio": "tall"
+                                    },
+                                    "buttons": [
+                                        {
+                                            "title": "more info",
+                                            "type": "web_url",
+                                            "url": "https://www.moovrika.com/m/4082",
+                                            "webview_height_ratio": "tall"
+                                        }
+                                    ]
                                 }
                            ]
                        }
